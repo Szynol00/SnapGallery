77add7bd9d1a93fecc3eaf315f03dcaacab141c3
ArturPas <pasartur108@gmail.com>
2024-02-20 06:47:13 +0100

    Revert "."
    This reverts commit f8fa190d0e68fb1b72c226d0c35c5eef90ed0d4e.

--- app/src/main/java/com/example/snapgallery/activity/MainActivity.kt
+++ app/src/main/java/com/example/snapgallery/activity/MainActivity.kt
@@ -96,7 +96,7 @@ class MainActivity : AppCompatActivity() {
         timer.start()
         countClicks++
 
-        if (countClicks >= 0) {
+        if (countClicks >= 5) {
             // Vibrate for 500 milliseconds
             val v = getSystemService(VIBRATOR_SERVICE) as Vibrator
 
@@ -107,6 +107,9 @@ class MainActivity : AppCompatActivity() {
                         VibrationEffect.DEFAULT_AMPLITUDE
                     )
                 )
+
+                val intent = Intent(this, HiddenMultimedia::class.java)
+                this.startActivity(intent)
             } else v.vibrate(500)
         }
     }
